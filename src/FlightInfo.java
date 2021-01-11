e9b5b78423898cdeb2548af9a74f43a1dda12146
991225574 <991225574@qq.com>
2021-01-11 16:08:08 +0800

    提交

---
+++ src/FlightInfo.java
@@ -0,0 +1,40 @@
+import java.util.Date;
+
+public class FlightInfo {
+    private String id;
+    private String fromCity;    //始发站
+    private String toCity;      //到达站
+    private Date fromDate;      //出发时间
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFromCity() {
+        return fromCity;
+    }
+
+    public void setFromCity(String fromCity) {
+        this.fromCity = fromCity;
+    }
+
+    public String getToCity() {
+        return toCity;
+    }
+
+    public void setToCity(String toCity) {
+        this.toCity = toCity;
+    }
+
+    public Date getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(Date fromDate) {
+        this.fromDate = fromDate;
+    }
+}
